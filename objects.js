4d3c67c5af50c03e7e3c497511fa6b45be5aa498
Patrick Cullinane <cullinap@gmail.com>
2025-03-22 07:44:31 -0400

    add random spawn house

--- objects.js
+++ objects.js
@@ -2,7 +2,20 @@ obj = {x:700,y:225}
 
 
 function generateObject() {
-    obj.x = Math.floor(Math.random() * (750 - 650 + 1)) + 700;
+    // get random value between 0-100
+    // floor it to get just the integer
+    // add 700 to keep it in the 700 area
+    // math random = 0.604 * 101 = 60.4, floor = 60, + 700
+    let randValue = Math.random();
+    let locDeterminer = 0;
+
+    if (randValue <= 0.5) {
+        locDeterminer = 300;
+    } else {
+        locDeterminer = 650;
+    }
+    obj.x = Math.floor(Math.random() * (750 - 650 + 1)) + locDeterminer;
+    
 
     // find the x,y >= 700 -/+ TR (700-35) & (700+35)
     let leftPoint = terrain.find(t => t.x >= obj.x - terrainResolution);
